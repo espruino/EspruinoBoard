d4bb8ede512ce18e1831b87060369c78473754c3
Gordon Williams <gw@pur3.co.uk>
2015-09-21 11:39:28 +0100

    tool for making thumbnails

---
+++ Pico/Adaptors/bin/make_thumbnails.sh
@@ -0,0 +1,33 @@
+cd `dirname $0`/../eagle
+EAGLE=/opt/eagle-6.5.0/bin/eagle
+
+rm -f script.scr foo.brd
+
+for f in *.brd; do
+  BRDFILE=`pwd`/$f
+  IMGFILE=`pwd`/`basename $f .brd`.png
+  echo Thumbnail $f
+  echo -----------------------
+#  echo EDIT $f >> script.scr
+  echo "DISPLAY ALL;" >> script.scr
+  echo "GROUP ALL;" >> script.scr
+  echo "DELETE SIGNALS;DELETE (c > 0 0);" >> script.scr
+  echo "PASTE R0 $BRDFILE (0 0);" >> script.scr
+  echo "DISPLAY ALL;" >> script.scr
+  echo "RATSNEST;" >> script.scr  
+  echo "EXPORT IMAGE $IMGFILE 600;" >> script.scr
+done
+
+echo CLOSE >> script.scr
+echo QUIT >> script.scr
+
+rm -f *.png
+cat script.scr
+$EAGLE -Sscript.scr foo.brd
+
+for f in *.brd; do  
+  IMGFILE=`pwd`/`basename $f .brd`.png
+  echo Resizing $IMGFILE
+  mogrify -resize 15% -negate $IMGFILE
+done
+
